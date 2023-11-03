4c3a721377365e8b9a5751607c2bcde68584dab9
wahyupambudi <wahyupambudi823@gmail.com>
2023-11-03 08:36:56 +0700

    add deposit, withdraw

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 // const { Get, Insert, GetByPK, Update, Delete } = require('../controller/transaction.controller')
-const {Get, Insert, GetByPK} = require('../controller/transaction.controller')
-const { CheckPostTransaction } = require('../middleware/middleware')
+const {Get, Insert, GetByPK, Deposit, Withdraw} = require('../controller/transaction.controller')
+const { CheckPostTransaction, CheckPostDeposit } = require('../middleware/middleware')
 
 router.get('/', Get)
 router.get('/:id', GetByPK)
 router.post('/', CheckPostTransaction, Insert)
+router.post('/deposit', CheckPostDeposit, Deposit)
+router.post('/withdraw', CheckPostDeposit, Withdraw)
 // router.put('/:transaction', Update)
 // router.delete('/:transaction', Delete)
 module.exports = router
